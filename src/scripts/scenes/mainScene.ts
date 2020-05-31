cd922411a872a72279d423dc66f10c67fd016ce0
Wielqus <Wielqus@gmail.com>
2020-05-31 21:28:10 +0200

    Basic example map

--- src/scripts/scenes/mainScene.ts
+++ src/scripts/scenes/mainScene.ts
@@ -7,9 +7,23 @@ export default class MainScene extends Phaser.Scene {
     super({ key: 'MainScene' })
   }
 
+  preload(){
+    this.load.image("terrain", "./assets/image/terrain_atlas.png");
+    this.load.tilemapTiledJSON("map", "./assets/maps/mappy.json");
+
+  }
 
   create() {
     this.fpsText = new FpsText(this)
+
+    let mappy = this.add.tilemap("map");
+
+    let terrain = mappy.addTilesetImage("terrain_atlas", "terrain");
+
+    //layers
+    let botLayer = mappy.createStaticLayer("bot", [terrain], 0, 0).setDepth(-1);
+    let topLayer = mappy.createStaticLayer("top", [terrain], 0, 0);
+
 
     // display the Phaser.VERSION
     this.add
