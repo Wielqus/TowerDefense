e4d519b280ef64c10b1c2f1e313c5bb436a97327
Wielqus <Wielqus@gmail.com>
2020-05-31 22:59:03 +0200

    Camera movement

--- src/scripts/scenes/mainScene.ts
+++ src/scripts/scenes/mainScene.ts
@@ -2,6 +2,7 @@ import FpsText from '../objects/fpsText'
 
 export default class MainScene extends Phaser.Scene {
   fpsText: Phaser.GameObjects.Text
+  controls: Phaser.Cameras.Controls.FixedKeyControl
 
   constructor() {
     super({ key: 'MainScene' })
@@ -14,16 +15,24 @@ export default class MainScene extends Phaser.Scene {
   }
 
   create() {
-    
-
     const map = this.make.tilemap({key: 'map'});
-
     const terrain = map.addTilesetImage("terrain_atlas", "terrain");
 
     //layers
     const botLayer = map.createStaticLayer("bot", [terrain], 0, 0)
     this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
-    this.cameras.main.startFollow(this.input.activePointer)
+  
+    // Camera movement settings
+    const controlConfig = {
+        camera: this.cameras.main,
+        left: this.input.keyboard.addKey('A'),
+        right: this.input.keyboard.addKey('D'),
+        up: this.input.keyboard.addKey('W'),
+        down: this.input.keyboard.addKey('S'),
+        speed: 0.5
+    };
+    this.controls = new Phaser.Cameras.Controls.FixedKeyControl(controlConfig);
+
 
     // display the Phaser.VERSION
     this.add
@@ -32,11 +41,12 @@ export default class MainScene extends Phaser.Scene {
         fontSize: 24
       })
       .setOrigin(1, 0)
-      
-      this.fpsText = new FpsText(this)
+
+      this.fpsText = new FpsText(this)  
   }
 
   update() {
-    this.fpsText.update()
+    this.fpsText.update(this.time) // Update Fps text
+    this.controls.update(50); //Update camera
   }
 }
