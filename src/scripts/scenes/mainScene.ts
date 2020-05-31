5ee5db4da6c64fbc2f6a63fc240bb73f965a5e58
Wielqus <Wielqus@gmail.com>
2020-05-31 22:43:13 +0200

    New map. Camera follow cursor

--- src/scripts/scenes/mainScene.ts
+++ src/scripts/scenes/mainScene.ts
@@ -9,27 +9,21 @@ export default class MainScene extends Phaser.Scene {
 
   preload(){
     this.load.image("terrain", "./assets/image/terrain_atlas.png");
-    this.load.tilemapTiledJSON("map", "./assets/maps/mappy.json");
+    this.load.tilemapTiledJSON("map", "./assets/maps/map.json");
 
   }
 
   create() {
-    this.fpsText = new FpsText(this)
+    
 
-    const map = this.add.tilemap("map");
+    const map = this.make.tilemap({key: 'map'});
 
     const terrain = map.addTilesetImage("terrain_atlas", "terrain");
 
     //layers
-    const botLayer = map.createStaticLayer("bot", [terrain], 0, 0).setDepth(-1);
-    const topLayer = map.createStaticLayer("top", [terrain], 0, 0);
-
-    var cursors = this.input.keyboard.addKeys({
-      up: 'up',
-      down: 'down',
-      left: 'left',
-      right: 'right'
-  });
+    const botLayer = map.createStaticLayer("bot", [terrain], 0, 0)
+    this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+    this.cameras.main.startFollow(this.input.activePointer)
 
     // display the Phaser.VERSION
     this.add
@@ -38,30 +32,11 @@ export default class MainScene extends Phaser.Scene {
         fontSize: 24
       })
       .setOrigin(1, 0)
+      
+      this.fpsText = new FpsText(this)
   }
 
   update() {
     this.fpsText.update()
-    var cursors = this.input.keyboard.createCursorKeys();
-    if (this.input.keyboard.addKey('D').isDown)
-    {
-        this.cameras.main.x -= 10;
-    }
-
-    if (this.input.keyboard.addKey('A').isDown)
-    {
-        this.cameras.main.x += 10;
-    }
-
-    if (this.input.keyboard.addKey('S').isDown)
-    {
-        this.cameras.main.y -= 10;
-    }
-
-    if (this.input.keyboard.addKey('W').isDown)
-    {
-        this.cameras.main.y += 10;
-    }
-
   }
 }
