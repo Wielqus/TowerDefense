6145b0e1e05af9431c412a72ce4ae6d4977e0787
Wielqus <Wielqus@gmail.com>
2020-05-31 21:13:11 +0200

    Change project template

---
+++ src/scripts/scenes/mainScene.ts
@@ -0,0 +1,26 @@
+import FpsText from '../objects/fpsText'
+
+export default class MainScene extends Phaser.Scene {
+  fpsText: Phaser.GameObjects.Text
+
+  constructor() {
+    super({ key: 'MainScene' })
+  }
+
+
+  create() {
+    this.fpsText = new FpsText(this)
+
+    // display the Phaser.VERSION
+    this.add
+      .text(this.cameras.main.width - 15, 15, `Phaser v${Phaser.VERSION}`, {
+        color: '#000000',
+        fontSize: 24
+      })
+      .setOrigin(1, 0)
+  }
+
+  update() {
+    this.fpsText.update()
+  }
+}
