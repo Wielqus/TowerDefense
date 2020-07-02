4a5c660fbaab26afbea65c1ce2700ee2476e468e
Wielqus <Wielqus@gmail.com>
2020-07-02 21:55:14 +0200

    New map

--- src/collections/Maps.ts
+++ src/collections/Maps.ts
@@ -19,6 +19,27 @@ export let maps = [
                 "name": "Objects",
             }
         ]
+    },
+    {
+        "name":     "Kreta",
+        "jsonFile": "./assets/maps/Kreta3.json",
+        "tileSets":  [
+            {
+                "name":     "terrain_atlas",
+                "source":   "./assets/image/terrain_atlas.png"
+            }
+        ],
+        "layers": [
+            {
+                "name": "Bot",
+            },
+            {
+                "name": "Top",
+            },
+            {
+                "name": "Objects",
+            }
+        ]
     }
 
 
