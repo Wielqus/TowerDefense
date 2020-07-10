0e16a591afe2d10495d231a451c804620d49ed19
Wielqus <Wielqus@gmail.com>
2020-07-10 22:00:42 +0200

    Fix bug (Problem with detect correct fields)

--- src/scripts/objects/MapMarker.ts
+++ src/scripts/objects/MapMarker.ts
@@ -1,14 +1,14 @@
 import { Vector } from "matter"
 import { Tilemaps } from 'phaser';
 
-export default class MapMarker extends Phaser.GameObjects.GameObject{
+export default class MapMarker extends Phaser.GameObjects.GameObject {
     scene: Phaser.Scene
     marker: Phaser.GameObjects.Graphics
     map
     availableTiles: Array<Tilemaps.Tile>
 
 
-    constructor(scene: Phaser.Scene, map, availableTiles: Array<Tilemaps.Tile> = []){
+    constructor(scene: Phaser.Scene, map, availableTiles: Array<Tilemaps.Tile> = []) {
         super(scene, "MapMarker")
         this.scene = scene
         this.marker = this.scene.add.graphics()
@@ -16,52 +16,60 @@ export default class MapMarker extends Phaser.GameObjects.GameObject{
         this.availableTiles = availableTiles
         this.create()
         scene.events.on('update', (time, delta) => {
-            this.update()
+            this.update(time, delta)
         });
-        
-    }
-
-    destructor(){
-
-    }
-    
-    preload(){
 
     }
 
-    create(){
+    destructor() {
 
     }
 
-    update(){
-        if(this.scene){ 
+    preload() {
+
+    }
+
+    create() {
+
+    }
+
+    findTileNeighbors(mainTile: Phaser.Tilemaps.Tile) {
+        const rightTile: Phaser.Tilemaps.Tile = this.map.map.findTile((tile: Phaser.Tilemaps.Tile) => {
+            return tile.x === mainTile.x + 1 && tile.y === mainTile.y
+        })
+        const downTile: Phaser.Tilemaps.Tile = this.map.map.findTile((tile: Phaser.Tilemaps.Tile) => {
+            return tile.x === mainTile.x && tile.y === mainTile.y + 1
+        })
+        const rightDownTile: Phaser.Tilemaps.Tile = this.map.map.findTile((tile: Phaser.Tilemaps.Tile) => {
+            return tile.x === mainTile.x + 1 && tile.y === mainTile.y + 1
+        })
+
+        return [mainTile, rightTile, downTile, rightDownTile]
+    }
+
+    update(time, delta) {
+        if (this.scene) {
+            this.marker.clear();
             const worldPoint = this.scene.input.activePointer.positionToCamera(this.scene.cameras.main);
-            console.log(this.scene.input.activePointer.moveTime)
-            this.scene.input.activePointer.x = worldPoint["x"]
-            this.scene.input.activePointer.y = worldPoint["y"]
             const pointerTile: Tilemaps.Tile = this.map.map.worldToTileXY(worldPoint["x"], worldPoint["y"]);
             const snappedWorldPoint = this.map.map.tileToWorldXY(pointerTile.x, pointerTile.y);
             this.marker.setPosition(snappedWorldPoint.x, snappedWorldPoint.y);
             const tile = this.map.map.getTileAt(pointerTile.x, pointerTile.y)
-            
-            this.marker.clear();
-            if(tile){
-                const tiles = [tile, this.map.map.getTileAtWorldXY(pointerTile.x + this.map.map.tileWidth, pointerTile.y), 
-                    this.map.map.getTileAtWorldXY(pointerTile.x, pointerTile.y + this.map.map.tileHeight), this.map.map.getTileAtWorldXY(pointerTile.x + this.map.map.tileWidth, pointerTile.y + this.map.map.tileHeight)]
-               // console.log(tiles)
-                    const found = tiles.map(tile => {
-                    if(tile){
-                        return tile.properties.towerPlace
+            if (tile) {
+                const tiles = this.findTileNeighbors(tile)
+                const found = tiles.every(tile => {
+                    if (tile) {
+                        return tile.properties.towerPlace === true
                     }
                     return false
                 })
-                //console.log(found)
-                if(found){
+                if (found) {
                     this.marker.fillStyle(0x008000, 0.5)
-                }else{
+
+                } else {
                     this.marker.fillStyle(0xFF0000, 0.5)
                 }
-            }else{
+            } else {
                 this.marker.fillStyle(0xFF0000, 0.5)
             }
             this.marker.fillRect(0, 0, 2 * this.map.map.tileWidth, 2 * this.map.map.tileHeight);
