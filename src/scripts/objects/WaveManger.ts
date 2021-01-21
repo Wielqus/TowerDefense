b587d33a3fcd264fd7db07d6c7623ebcee95f059
Jakub Wielgus <38864045+Wielqus@users.noreply.github.com>
2021-01-21 22:46:26 +0100

    Start wave and counter

--- src/scripts/objects/WaveManger.ts
+++ src/scripts/objects/WaveManger.ts
@@ -1,5 +1,8 @@
 import { Scene } from "phaser";
 import EventDispatcher from '../EventDispatcher'
+import IMonster from '../Interfaces/IMonster'
+import Monster from './Monster'
+import Map from './Map'
 
 export default class WafeManger{
     time: integer
@@ -7,11 +10,15 @@ export default class WafeManger{
     scene: Scene
     timer: any
     emitter: EventDispatcher
+    waves: Array<Array<IMonster>>
+    map: Map
 
-    constructor(scene: Scene){
+    constructor(scene: Scene, waves: Array<Array<IMonster>>, map: Map){
         this.round = 1;
         this.time = 15;
         this.scene = scene
+        this.waves = waves
+        this.map = map
         this.emitter = EventDispatcher.getInstance();
         this.emitter.on('stop_counting', this.stopCounting, this)
         this.startCounting()
@@ -30,14 +37,28 @@ export default class WafeManger{
         this.time = 0
         this.timer.remove();
         this.emitter.emit("change_time", this.time, this.round)
+        this.startWave()
     }
 
     setTime(){
         this.time = this.time - 1
+        if(this.time === 0){
+            this.stopCounting()
+            return
+        }
         this.emitter.emit("change_time", this.time, this.round)
     }
 
-    
-
+    startWave(){
+        const wave = this.waves.pop()
+        if (wave) {
+            wave.forEach((monster) => {
+                
+                setTimeout(() => {
+                    const monsterInstance = new Monster(this.scene, this.map.getRandomPath(), monster)
+                }, Math.random() * 5000)
+            })
+        }
+    }
 
 }
--- src/scripts/scenes/mainScene.ts
+++ src/scripts/scenes/mainScene.ts
@@ -2,6 +2,7 @@
 import Map from '../objects/Map'
 import EventDispatcher from '../EventDispatcher'
 import WaveManager from '../objects/WaveManger'
+import {monsters} from '../../collections/Monsters'
 
 
 export default class MainScene extends Phaser.Scene {
@@ -18,12 +19,18 @@ export default class MainScene extends Phaser.Scene {
 
   init(data) {
     this.map = new Map(this, data.map)
-    this.waveManager = new WaveManager(this)
+    this.waveManager = new WaveManager(this, this.map.mapData.waves, this.map)
     
   }
 
   preload() {
     this.emitter.emit("main_scene_preload", this)
+    for (let [key, monster] of Object.entries(monsters)) {
+      this.load.spritesheet(monster.name, `./assets/monsters/${monster.source}`, {
+        frameWidth: monster.width,
+        frameHeight: monster.height
+      })
+    }
   }
 
   create() {
